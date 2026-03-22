cc4291f36e28665421be3cb0f87b6fa9d893984d
SamCorp <Manhcuong5311@gmail.com>
2026-03-22 20:29:56 +0700

    Initial Commit

---
+++ Serein/SereinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SereinApp.swift
+//  Serein
+//
+//  Created by Sam Manh Cuong on 22/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SereinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
